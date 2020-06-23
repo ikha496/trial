af8451883d682c302abed85793fd6e82f560db2f
Ibrahim Khaled <61571493+ikha496@users.noreply.github.com>
2020-06-23 16:57:23 +0200

    Update js.js

--- sub/js.js
+++ sub/js.js
@@ -11,32 +11,32 @@ for(var i=0; inputElements[i]; ++i){
 
     if(sum==0)
         {
-            window.open("../result none.html","_self");
+            window.open("result none.html","_self");
 
         }
     else if(sum<=4)
         {
-            window.open("../result low.html","_self");
+            window.open("result low.html","_self");
 
         }
     else if (sum==5)
         {
-            window.open("../result medium.html","_self");
+            window.open("result medium.html","_self");
         }
     else if (sum>=5)
         {
-            window.open("../result high.html","_self");
+            window.open("result high.html","_self");
         }
 }
 
 function openadvice3azl(){
 var tt = setInterval(function() {
-    window.open ('../advice3azl.html','_self',false)
+    window.open ('advice3azl.html','_self',false)
 },5000);
 }
 
 function openadvicewekaya(){
 var tt = setInterval(function() {
-    window.open ('../advicewekaya.html','_self',false)
+    window.open ('advicewekaya.html','_self',false)
 },5000);
 }
